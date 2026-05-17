ec6afeafe076f3ec4c371a37598360630d59e614
Rohini Kote <koterohini563@gmail.com>
2026-05-17 16:36:38 +0530

    backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import express from 'express';
+import cookieParesr from 'cookie-parser';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors({
+    origin: process.env.CORS_ORIGINS,
+    credentials: true
+}))
+
+app.use(bodyParser.json({ limit : "4kb"}));
+app.use(bodyParser.urlencoded({ extended: true, limit: "4kb" }));
+app.use(express.static('public'));
+
+app.use(cookieParesr());
+
+mongoose.connect(`${process.env.DB_PATH}/${process.env.DB_NAME}`)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`http://localhost:` + PORT);
+        });
+    })
+    .catch(err=>{
+        console.log(err);
+    })
